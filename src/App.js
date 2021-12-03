f0b969287c005afe3cdb635c5af35e1183167b42
Abhay189 <sabhay2001@gmail.com>
2021-12-02 21:56:22 -0700

    hi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import './NavigBar'
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigbar from './NavigBar';
+import { Accordion, Col, Container, Row } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigbar/>
+      <Container fluid className="mainContainer">
+        <Row>
+          <Col className="leftCol">
+          <div class="sidenav .d-none">
+            <h1>Catagories</h1>
+            <Accordion>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Clothing Type</Accordion.Header>
+                <Accordion.Body>
+                 
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Size</Accordion.Header>
+                <Accordion.Body>
+                  
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2">
+                <Accordion.Header>Price</Accordion.Header>
+                <Accordion.Body>
+                  
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="3">
+                <Accordion.Header>Brand</Accordion.Header>
+                <Accordion.Body>
+                  
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+          
+            {/* <Button >Heyy</Button> */}
+          </Col>
+          <Col className="rightCol">
+            <Button >Heyy</Button>
+          </Col>
+        </Row>
+      </Container>
+
+
+
+      <body>
+      
+      </body>
     </div>
   );
 }
