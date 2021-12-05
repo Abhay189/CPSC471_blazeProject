de9990b45efca93f6df75652761c34b604af9c5c
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2021-12-04 20:47:26 -0700

    Got the sidebar working
    Couldn't get the other pages to open

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
+
 import './CSS/App.css';
 import './NavigBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigbar from './NavigBar';
-import { Col, Container, Row } from 'react-bootstrap';
-import Sidepanel from './Sidepanel';
-import RightColumn from './RightColumn';
+import Admin from './Admin';
 
 function App() {
   return (
     <div className="App">
-      <Navigbar/>
-      <Container fluid className="mainContainer">
-        <Row>
-          <Col className="leftCol" lg = {4} sm= {12}>
-            <Sidepanel/>
-          </Col>
-            <Col className = "rightColumn">
-              <RightColumn/>
-          </Col>
-        </Row>
-      </Container>
+      <Admin/>
     </div>
   );
 }
