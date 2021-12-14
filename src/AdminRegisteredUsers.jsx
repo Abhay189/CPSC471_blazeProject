385a4ec59c4cc7465cdce5145b2c0c4c5702f4b2
Dustin <binhng1908@gmail.com>
2021-12-15 02:42:50 +0700

    adding the api functions

--- src/AdminRegisteredUsers.jsx
+++ src/AdminRegisteredUsers.jsx
@@ -1,10 +1,32 @@
-import {Table } from "react-bootstrap";
-import './CSS/AdminTable.css'
+import React from "react";
+import { render } from "@testing-library/react";
+import { Table } from "react-bootstrap";
+import "./CSS/AdminTable.css";
 
-function AdminRegisteredUser(){
-    return(
-        <div class = "AdminProductTable">
-        <Table striped bordered hover size="sm"> 
+class AdminRegisteredUser extends React.Component {
+  componentDidMount() {
+    const axios = require("axios");
+    // Make a request for a user with a given ID
+    axios
+      .get("https://localhost:44392/api/users")
+      .then(function (response) {
+        // handle success
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        console.log("hither");
+        // always executed
+      });
+  }
+
+  render() {
+    return (
+      <div class="AdminProductTable">
+        <Table striped bordered hover size="sm">
           <thead>
             <tr>
               <th>Product id</th>
@@ -15,26 +37,66 @@ function AdminRegisteredUser(){
           <tbody>
             <tr>
               <td>1</td>
-              <td class = "align">
+              <td class="align">
                 <tr>LoginID:1023</tr>
                 <tr>Name: bob</tr>
                 <tr>Phone Number:</tr>
               </td>
-              <td><button>Delete</button></td>
+              <td>
+                <button>Delete</button>
+              </td>
             </tr>
             <tr>
               <td>2</td>
-              <td class = "align">
-              <tr>LoginID:3232</tr>
+              <td class="align">
+                <tr>LoginID:3232</tr>
                 <tr>Name: james</tr>
                 <tr>Phone Number:</tr>
               </td>
-              <td><button>Delete</button></td>
+              <td>
+                <button>Delete</button>
+              </td>
             </tr>
           </tbody>
         </Table>
-        </div>
-        )
-    }
-    
-    export default AdminRegisteredUser;
+      </div>
+    );
+  }
+}
+// function AdminRegisteredUser(){
+//     return(
+//         <div class = "AdminProductTable">
+//         <Table striped bordered hover size="sm">
+//           <thead>
+//             <tr>
+//               <th>Product id</th>
+//               <th>Product</th>
+//               <th>Delete</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             <tr>
+//               <td>1</td>
+//               <td class = "align">
+//                 <tr>LoginID:1023</tr>
+//                 <tr>Name: bob</tr>
+//                 <tr>Phone Number:</tr>
+//               </td>
+//               <td><button>Delete</button></td>
+//             </tr>
+//             <tr>
+//               <td>2</td>
+//               <td class = "align">
+//               <tr>LoginID:3232</tr>
+//                 <tr>Name: james</tr>
+//                 <tr>Phone Number:</tr>
+//               </td>
+//               <td><button>Delete</button></td>
+//             </tr>
+//           </tbody>
+//         </Table>
+//         </div>
+//         )
+//     }
+
+export default AdminRegisteredUser;
--- src/RightColumn.jsx
+++ src/RightColumn.jsx
@@ -1,44 +1,79 @@
-import React from 'react';
+import React from "react";
 import ClothesCard from "./Cards";
-import image from './image.png';
-import './CSS/RightColumn.css';
+import image from "./image.png";
+import "./CSS/RightColumn.css";
 
+let productArray = new Array();
 
 class RightColumn extends React.Component {
+  constructor(props) {
+    super(props);
+    // let productArray = new Array();
+    this.state = {
+      // productArray: new Array()
+    };
+    this.callAPI = this.callAPI.bind(this);
+    this.callAPI();
+  }
 
-  render(){
-    return(
-      <div>            
-          <div className="heading">
-            <h1 id= "men_tops">Mens Tops</h1>
-          </div>
-          <div className="cardHolder">
-            <ClothesCard className="card" source={image}/>
-            <ClothesCard className="card" source={image}/>
-            <ClothesCard className="card" source={image}/>
-            <ClothesCard className="card" source={image}/>
-            <ClothesCard className="card" source={image}/>
-            <ClothesCard className="card" source={image}/>
-          </div>
-          <div className="heading">
-            <h1 id= "men_lower">Mens Bottom</h1>
-          </div>
-          <div className="cardHolder">
-            <ClothesCard className="card" source={image}/>
-            <ClothesCard className="card" source={image}/>
-            <ClothesCard className="card" source={image}/>
-            <ClothesCard className="card" source={image}/>
-            <ClothesCard className="card" source={image}/>
-            <ClothesCard className="card" source={image}/>
-          </div>
+  callAPI() {
+    const axios = require("axios");
+    // Make a request for a user with a given ID
+    axios
+      .get("https://localhost:44392/api/products")
+      .then(function (response) {
+        for (var i = 0; i < response.data.length; i++) {
+          productArray.push(response.data[i]);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+        // console.log(productArray[0]);
+      });
+
+    // console.log(productArray);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="heading">
+          <h1 id="men_tops">Mens Tops</h1>
+        </div>
+        <div className="cardHolder">
+          <ClothesCard
+            className="card"
+            source={image}
+            // productname={productArray[0].name}
+            // description={productArray[0].descr}
+          />
+          <ClothesCard className="card" source={image} />
+          <ClothesCard className="card" source={image} />
+          <ClothesCard className="card" source={image} />
+          <ClothesCard className="card" source={image} />
+          <ClothesCard className="card" source={image} />
+        </div>
+        <div className="heading">
+          <h1 id="men_lower">Mens Bottom</h1>
+        </div>
+        <div className="cardHolder">
+          <ClothesCard className="card" source={image} />
+          <ClothesCard className="card" source={image} />
+          <ClothesCard className="card" source={image} />
+          <ClothesCard className="card" source={image} />
+          <ClothesCard className="card" source={image} />
+          <ClothesCard className="card" source={image} />
+        </div>
       </div>
-    )
+    );
   }
 }
 
 // function RightColumn(){
 //     return(
-//         <div>            
+//         <div>
 //             <div className="heading">
 //               <h1 id= "men_tops">Mens Tops</h1>
 //             </div>
