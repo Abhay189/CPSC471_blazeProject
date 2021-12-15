4a7f23b5187494399a087f3fe4ec9d776ca30390
Abhay189 <sabhay2001@gmail.com>
2021-12-15 11:23:27 -0700

    AdminInventoryProducts function to class update

--- src/AdminInventoryProducts.jsx
+++ src/AdminInventoryProducts.jsx
@@ -1,41 +1,82 @@
+import React from "react";
 import {Table} from "react-bootstrap";
 import './CSS/AdminTable.css'
 
-function AdminInventoryProducts(){
-  return(
-    <div class = "AdminProductTable">
-      <Table striped bordered hover size="sm"> 
-        <thead>
-          <tr>
-            <th>Product id</th>
-            <th>Product</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td class = "align">
-              <tr>Name:</tr>
-              <tr>Price:</tr>
-              <tr>Supplier:</tr>
-            </td>
-            <td><button>Delete</button></td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td class = "align">
-              <tr>Name:</tr>
-              <tr>Price:</tr>
-              <tr>Supplier:</tr>
-            </td>
-            <td><button>Delete</button></td>
-          </tr>
+class AdminInventoryProducts extends React.Component {
 
-        </tbody>
-      </Table>
-      </div>
-    )
+  
+  render(){
+    return(
+      <div class = "AdminProductTable">
+        <Table striped bordered hover size="sm"> 
+          <thead>
+            <tr>
+              <th>Product id</th>
+              <th>Product</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td class = "align">
+                <tr>Name:</tr>
+                <tr>Price:</tr>
+                <tr>Supplier:</tr>
+              </td>
+              <td><button>Delete</button></td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td class = "align">
+                <tr>Name:</tr>
+                <tr>Price:</tr>
+                <tr>Supplier:</tr>
+              </td>
+              <td><button>Delete</button></td>
+            </tr>
+  
+          </tbody>
+        </Table>
+        </div>
+      )
+  }
 }
+// function AdminProductsDeletion(){
+//   return(
+//     <div class = "AdminProductTable">
+//       <Table striped bordered hover size="sm"> 
+//         <thead>
+//           <tr>
+//             <th>Product id</th>
+//             <th>Product</th>
+//             <th>Delete</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <td>1</td>
+//             <td class = "align">
+//               <tr>Name:</tr>
+//               <tr>Price:</tr>
+//               <tr>Supplier:</tr>
+//             </td>
+//             <td><button>Delete</button></td>
+//           </tr>
+//           <tr>
+//             <td>2</td>
+//             <td class = "align">
+//               <tr>Name:</tr>
+//               <tr>Price:</tr>
+//               <tr>Supplier:</tr>
+//             </td>
+//             <td><button>Delete</button></td>
+//           </tr>
+
+//         </tbody>
+//       </Table>
+//       </div>
+//     )
+// }
 
 export default AdminInventoryProducts;
