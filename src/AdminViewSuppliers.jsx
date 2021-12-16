d68890c0823b643b2c7f6a599e0a034998804a98
Dustin <binhng1908@gmail.com>
2021-12-16 23:46:53 +0700

    Return array of API URL to call

--- src/AdminViewSuppliers.jsx
+++ src/AdminViewSuppliers.jsx
@@ -1,71 +1,81 @@
 import React from "react";
-import {Table} from "react-bootstrap";
-import './CSS/AdminTable.css'
+import { Table } from "react-bootstrap";
+import "./CSS/AdminTable.css";
 
-
-class AdminViewSupplier extends React.Component{
-
+class AdminViewSupplier extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       supplierArray: [],
-      error: null
-    }
+      error: null,
+    };
   }
-  
+
   buildList = (data) => {
     this.setState({
-      supplierArray: data
-    })
-  }
+      supplierArray: data,
+    });
+  };
 
   componentDidMount() {
-    let url = "https://localhost:" + this.props.API_URL + "/api/suppliers"; 
+    let url = "https://localhost:" + this.props.API_URL + "/api/suppliers";
     fetch(url)
-    .then(response => response.json())
-    .then(this.buildList)
-    .catch()
+      .then((response) => response.json())
+      .then(this.buildList)
+      .catch();
   }
 
-  render(){
-    let suppliers = []
-    if(this.state.supplierArray.length > 0){
-      for(let i = 0; i<this.state.supplierArray.length; i++){
+  deleteBtn() {
+    let url = "https://localhost:" + this.props.API_URL + "/api/suppliers";
+    console.log(url);
+  }
+
+  render() {
+    let suppliers = [];
+    if (this.state.supplierArray.length > 0) {
+      for (let i = 0; i < this.state.supplierArray.length; i++) {
         suppliers.push(
           <tr>
             <td>{this.state.supplierArray[i].SupID}</td>
-            <td class = "align">
+            <td class="align">
               <tr>Name:{this.state.supplierArray[i].Name}</tr>
               <tr>Email:{this.state.supplierArray[i].Email}</tr>
-              <tr>Address:{this.state.supplierArray[i].Street} {this.state.supplierArray[i].City} {this.state.supplierArray[i].PostalCode}</tr>
+              <tr>
+                Address:{this.state.supplierArray[i].Street}{" "}
+                {this.state.supplierArray[i].City}{" "}
+                {this.state.supplierArray[i].PostalCode}
+              </tr>
               <tr>Phone Number:{this.state.supplierArray[i].PhoneNo}</tr>
             </td>
-            <td><button>Delete</button></td>
+            <td>
+              <button
+                onClick={this.deleteBtn(this.state.supplierArray[i].SupID)}
+              >
+                Delete
+              </button>
+            </td>
           </tr>
-        )
+        );
       }
-      return(
-        <div class = "AdminProductTable">
-            <Table striped bordered hover size="sm"> 
-              <thead>
-                <tr>
-                  <th>Supplier ID</th>
-                  <th>Supplier</th>
-                  <th>Delete</th>
-                </tr>
-              </thead>
-              <tbody>
-                {suppliers}
-              </tbody>
-            </Table>
-          </div>
+      return (
+        <div class="AdminProductTable">
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th>Supplier ID</th>
+                <th>Supplier</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>{suppliers}</tbody>
+          </Table>
+        </div>
       );
-    }
-    else{
-      return(
-          <div class = "AdminProductTable">
-            <h1>Loading...</h1>
-          </div>
+    } else {
+      return (
+        <div class="AdminProductTable">
+          <h1>Loading...</h1>
+        </div>
       );
     }
   }
--- src/Sidepanel.jsx
+++ src/Sidepanel.jsx
@@ -1,119 +1,213 @@
 import React from "react";
 import { Accordion, Button } from "react-bootstrap";
-import './CSS/Sidepanel.css'
+import "./CSS/Sidepanel.css";
 
-class Sidepanel extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            topcheck: false,
-            bottomcheck: false,
-            XLcheck: false,
-            Largecheck : false, 
-            Mediumcheck : false, 
-            smallcheck: false,
-            price1 : false,
-            price2check: false,
-            price3check: false,
-            price4check: false,
-            Black: false,
-            Blue: false,
-            Green: false,
-            Orange: false,
-            Pink: false,
-            Red: false,
-            White: false,
-            Yellow: false,
-        }
-        this.myfun = this.myfun.bind(this);
+class Sidepanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      CatTop: false,
+      CatBottom: false,
+      SizeXL: false,
+      SizeL: false,
+      SizeM: false,
+      SizeS: false,
+      Price1: false,
+      Price2: false,
+      Price3: false,
+      Price4: false,
+      ColorBlack: false,
+      ColorBlue: false,
+      ColorGreen: false,
+      ColorOrange: false,
+      ColorPink: false,
+      ColorRed: false,
+      ColorWhite: false,
+      ColorYellow: false,
+    };
+    this.myfun = this.myfun.bind(this);
+  }
+
+  myfun() {
+    this.setState({
+      CatTop: document.getElementById("top").checked,
+      CatBottom: document.getElementById("Bottom").checked,
+
+      SizeXL: document.getElementById("XL").checked,
+      SizeL: document.getElementById("L").checked,
+      SizeM: document.getElementById("M").checked,
+      SizeS: document.getElementById("S").checked,
+
+      Price1: document.getElementById("price1").checked,
+      Price2: document.getElementById("price2").checked,
+      Price3: document.getElementById("price3").checked,
+      Price4: document.getElementById("price4").checked,
+
+      ColorBlack: document.getElementById("Black").checked,
+      ColorBlue: document.getElementById("Blue").checked,
+      ColorGreen: document.getElementById("Green").checked,
+      ColorOrange: document.getElementById("Orange").checked,
+      ColorPink: document.getElementById("Pink").checked,
+      ColorRed: document.getElementById("Red").checked,
+      ColorWhite: document.getElementById("White").checked,
+      ColorYellow: document.getElementById("Yellow").checked,
+    });
+
+    let apiCalls = new Array();
+    for (let [key, value] of Object.entries(this.state)) {
+      if (key.includes("Cat") && value)
+        apiCalls.push(
+          `https://localhost:${this.props.API_URL}/api/productcat/${key.replace(
+            "Cat",
+            ""
+          )}`
+        );
+      else if (key.includes("Size") && value)
+        apiCalls.push(
+          `https://localhost:${
+            this.props.API_URL
+          }/api/productsize/${key.replace("Size", "")}`
+        );
+      else if (key.includes("Price") && value)
+        apiCalls.push(
+          `https://localhost:${
+            this.props.API_URL
+          }/api/productprice/${key.replace("Price", "")}`
+        );
+      else if (key.includes("Color") && value)
+        apiCalls.push(
+          `https://localhost:${
+            this.props.API_URL
+          }/api/productcolor/${key.replace("Color", "")}`
+        );
     }
 
-    myfun(){
-        this.setState({
-            topcheck: document.getElementById("top").checked,
-            bottomcheck: document.getElementById("Bottom").checked,
+    console.log(apiCalls);
+  }
 
-            XLcheck: document.getElementById("XL").checked,
-            Largecheck : document.getElementById("Large").checked, 
-            Mediumcheck : document.getElementById("Medium").checked, 
-            smallcheck: document.getElementById("Small").checked,
+  componentDidMount() {
+    this.myfun();
+  }
 
-            price1 : document.getElementById("price1").checked,
-            price2check: document.getElementById("price2").checked,
-            price3check: document.getElementById("price3").checked,
-            price4check: document.getElementById("price4").checked,
-
-            Black: document.getElementById("Black").checked,
-            Blue: document.getElementById("Blue").checked,
-            Green: document.getElementById("Green").checked,
-            Orange: document.getElementById("Orange").checked,
-            Pink: document.getElementById("Pink").checked,
-            Red: document.getElementById("Red").checked,
-            White: document.getElementById("White").checked,
-            Yellow: document.getElementById("Yellow").checked,
-        })
-        console.log(this.state);
-    }
-
-    componentDidMount(){
-        this.myfun();
-    }
-
-    render(){
-        return(
-            <div className="sidenav">
-            <h1>Catagories</h1>
-            <Accordion defaultActiveKey="0">
-            <Accordion.Item eventKey="0"> 
-                <Accordion.Header>Clothing Type</Accordion.Header>
-                <Accordion.Body>
-                <ul>
-                <li><input type="checkbox" id="top" name="topp" onClick={this.myfun} />Top</li>
-                <li><input type="checkbox" id="Bottom" name="Lower" onClick={this.myfun} />Bottom</li>
-                </ul>
-                </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-                <Accordion.Header>Size</Accordion.Header>
-                <Accordion.Body>
-                <ul>
-                <li><input id="XL" type="checkbox" onClick={this.myfun} />XL</li>
-                <li><input id="Large" type="checkbox" onClick={this.myfun} />Large</li>
-                <li><input id="Medium" type="checkbox" onClick={this.myfun} />Medium</li>
-                <li><input id="Small" type="checkbox" onClick={this.myfun} />Small</li>
-                </ul>
-                </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="2">
-                <Accordion.Header>Price</Accordion.Header>
-                <Accordion.Body>
-                <ul>
-                <li><input id="price1" type="checkbox" onClick={this.myfun} />$20-$100</li>
-                <li><input id="price2" type="checkbox" onClick={this.myfun} />$100-$250</li>
-                <li><input id="price3" type="checkbox" onClick={this.myfun} />$250-$500</li>
-                <li><input id="price4" type="checkbox" onClick={this.myfun} />$500+</li>
-                </ul>
-                </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="3">
-                <Accordion.Header>Color</Accordion.Header>
-                <Accordion.Body>
-                <ul>
-                <li><input id="Black" onClick={this.myfun} type="checkbox"/>Black</li>
-                <li><input id="Blue" onClick={this.myfun} type="checkbox"/>Blue</li>
-                <li><input id="Green" onClick={this.myfun} type="checkbox"/>Green</li>
-                <li><input id="Orange" onClick={this.myfun} type="checkbox"/>Orange</li>
-                <li><input id="Pink" onClick={this.myfun} type="checkbox"/>Pink</li>
-                <li><input id="Red" onClick={this.myfun} type="checkbox"/>Red</li>
-                <li><input id="White" onClick={this.myfun} type="checkbox"/>White</li>
-                <li><input id="Yellow" onClick={this.myfun} type="checkbox"/>Yellow</li>
-                </ul>
-                </Accordion.Body>
-            </Accordion.Item>
-            </Accordion>
-            <Button onClick={this.myfun} >Filter</Button>
-        </div>
-        )
-    }
+  render() {
+    return (
+      <div className="sidenav">
+        <h1>Catagories</h1>
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>Clothing Type</Accordion.Header>
+            <Accordion.Body>
+              <ul>
+                <li>
+                  <input
+                    type="checkbox"
+                    id="top"
+                    name="topp"
+                    onClick={this.myfun}
+                  />
+                  Top
+                </li>
+                <li>
+                  <input
+                    type="checkbox"
+                    id="Bottom"
+                    name="Lower"
+                    onClick={this.myfun}
+                  />
+                  Bottom
+                </li>
+              </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>Size</Accordion.Header>
+            <Accordion.Body>
+              <ul>
+                <li>
+                  <input id="XL" type="checkbox" onClick={this.myfun} />
+                  XL
+                </li>
+                <li>
+                  <input id="L" type="checkbox" onClick={this.myfun} />
+                  Large
+                </li>
+                <li>
+                  <input id="M" type="checkbox" onClick={this.myfun} />
+                  Medium
+                </li>
+                <li>
+                  <input id="S" type="checkbox" onClick={this.myfun} />
+                  Small
+                </li>
+              </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="2">
+            <Accordion.Header>Price</Accordion.Header>
+            <Accordion.Body>
+              <ul>
+                <li>
+                  <input id="price1" type="checkbox" onClick={this.myfun} />
+                  $20-$100
+                </li>
+                <li>
+                  <input id="price2" type="checkbox" onClick={this.myfun} />
+                  $100-$250
+                </li>
+                <li>
+                  <input id="price3" type="checkbox" onClick={this.myfun} />
+                  $250-$500
+                </li>
+                <li>
+                  <input id="price4" type="checkbox" onClick={this.myfun} />
+                  $500+
+                </li>
+              </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="3">
+            <Accordion.Header>Color</Accordion.Header>
+            <Accordion.Body>
+              <ul>
+                <li>
+                  <input id="Black" onClick={this.myfun} type="checkbox" />
+                  Black
+                </li>
+                <li>
+                  <input id="Blue" onClick={this.myfun} type="checkbox" />
+                  Blue
+                </li>
+                <li>
+                  <input id="Green" onClick={this.myfun} type="checkbox" />
+                  Green
+                </li>
+                <li>
+                  <input id="Orange" onClick={this.myfun} type="checkbox" />
+                  Orange
+                </li>
+                <li>
+                  <input id="Pink" onClick={this.myfun} type="checkbox" />
+                  Pink
+                </li>
+                <li>
+                  <input id="Red" onClick={this.myfun} type="checkbox" />
+                  Red
+                </li>
+                <li>
+                  <input id="White" onClick={this.myfun} type="checkbox" />
+                  White
+                </li>
+                <li>
+                  <input id="Yellow" onClick={this.myfun} type="checkbox" />
+                  Yellow
+                </li>
+              </ul>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        <Button onClick={this.myfun}>Filter</Button>
+      </div>
+    );
+  }
 }
 export default Sidepanel;
