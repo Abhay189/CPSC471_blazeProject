ceb20d1194f52457ac3ee511bf97bc1f8bac270b
Abhay189 <sabhay2001@gmail.com>
2021-12-17 14:52:44 -0700

    fixing

--- src/RightColumn.jsx
+++ src/RightColumn.jsx
@@ -19,7 +19,7 @@ class RightColumn extends React.Component {
   };
 
   componentDidMount() {
-    let url = "https://localhost:" + 44317 + "/api/products";
+    let url = `https://localhost:${this.props.API_URL}/api/products`;
     fetch(url)
       .then((response) => response.json())
       .then(this.buildList)
