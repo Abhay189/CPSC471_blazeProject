34b1db5ecce0048efb6891b4e33152abce810d6d
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2021-12-16 21:47:27 -0700

    update

--- src/AdminInventoryProducts.jsx
+++ src/AdminInventoryProducts.jsx
@@ -1,40 +1,97 @@
-import {Table} from "react-bootstrap";
-import './CSS/AdminTable.css'
+import React from "react";
+import { Table } from "react-bootstrap";
+import "./CSS/AdminTable.css";
 
-function AdminInventoryProducts(){
-  return(
-    <div class = "AdminProductTable">
-      <Table striped bordered hover size="sm"> 
-        <thead>
+class AdminInventoryProducts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productArray: [],
+      error: null,
+    };
+  }
+
+  buildList = (data) => {
+    this.setState({
+      productArray: data,
+    });
+    console.log(this.state.productArray);
+  };
+//this.props.API_URL
+  componentDidMount() {
+    let url = "https://localhost:" + 44309 + "/api/products";
+    fetch(url)
+      .then((response) => response.json())
+      .then(this.buildList)
+      .catch();
+  }
+
+  deleteBtn(PID, i) {
+    this.state.productArray.splice(i, 1);
+    this.setState({});
+
+    let url = `https://localhost:${this.props.API_URL}/api/products/${PID}`;
+    fetch(url, {
+      method: "DELETE",
+    })
+      .then((response) => console.log(response.json()))
+      .then()
+      .catch();
+  }
+
+  render() {
+    let products = [];
+    if (this.state.productArray.length > 0) {
+      for (let i = 0; i < this.state.productArray.length; i++) {
+        products.push(
           <tr>
-            <th>Product id</th>
-            <th>Product</th>
-            <th>Edit/Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td class = "align">
-              <tr>Name:</tr>
-              <tr>Price:</tr>
-              <tr>Supplier:</tr>
+            <td>{this.state.productArray[i].PID}</td>
+            <td className="align">
+              <tr>Name: {this.state.productArray[i].Name}</tr>
+              <tr>Descr: {this.state.productArray[i].Descr}</tr>
+              <tr>Price: {this.state.productArray[i].Price}$</tr>
+              <tr>PID: {this.state.productArray[i].PID}</tr>
+              <tr>Size: {this.state.productArray[i].Size}</tr>
+              <tr>Color: {this.state.productArray[i].Color}</tr>
             </td>
-            <td><button>Edit</button>    <button>Delete</button></td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td class = "align">
-              <tr>Name:</tr>
-              <tr>Price:</tr>
-              <tr>Supplier:</tr>
+            <td>
+            <button>Edit</button>        
+              <button
+                onClick={this.deleteBtn.bind(
+                  this,
+                  this.state.productArray[i].PID,
+                  i
+                )}
+              >
+                Delete
+              </button>
             </td>
-            <td><button>Edit</button>    <button>Delete</button></td>
           </tr>
-        </tbody>
-      </Table>
-      </div>
-    )
+        );
+      }
+
+      return (
+        <div class="AdminProductTable">
+          <Table striped bordered hover size="sm">
+            <thead>
+              <tr>
+                <th>Product ID</th>
+                <th>Product</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>{products}</tbody>
+          </Table>
+        </div>
+      );
+    } else {
+      return (
+        <div class="AdminProductTable">
+          <h1>Loading</h1>
+        </div>
+      );
+    }
+  }
 }
 
 export default AdminInventoryProducts;
