9a6da121269d6343be94146b147201f036062a99
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2021-12-16 21:52:38 -0700

    update

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,22 +1,18 @@
-import './CSS/Card.css';
+import "./CSS/Card.css";
 import { Card, Button } from "react-bootstrap";
 
-function ClothesCard(props){
-    return(
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src= {props.source} />
-        <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>$19.99</Card.Text>
-            <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-            </Card.Text>
-            <Button variant="primary">Add to Cart </Button>
-        </Card.Body>
-        </Card>
-
-    )
+function ClothesCard(props) {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={props.source} />
+      <Card.Body>
+        <Card.Title>{props.productname}</Card.Title>
+        <Card.Text>{props.productPrice}$</Card.Text>
+        <Card.Text>{props.description}</Card.Text>
+        <Button variant="primary">Add to Cart</Button>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default ClothesCard;
