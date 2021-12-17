03f4d4b530a40b7226f6b655ec57b44fbbb8c66a
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2021-12-16 21:49:47 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,86 +1,237 @@
-import './CSS/App.css';
-import './NavigBar'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from "react";
-import Navigbar from './NavigBar';
-import Adminsidepanel from './AdminSidepanel';
-import AdminSupplierAddForm  from './AdminSupplierForm';
-import AdminViewSupplier from './AdminViewSuppliers'
-import AdminInventoryProducts from './AdminInventoryProducts';
-import AdminRegisteredUser from './AdminRegisteredUsers';
-import AdminDiscount from './AdminDiscount';
-import LetsLogin from './TheLogin';
+import "./CSS/App.css";
+import "./NavigBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Navigbar from "./NavigBar";
+import { Col, Container, Row } from "react-bootstrap";
+import Sidepanel from "./Sidepanel";
+import RightColumn from "./RightColumn";
+import Adminsidepanel from "./AdminSidepanel";
+import AdminSupplierAddForm from "./AdminSupplierForm";
+import AdminViewSupplier from "./AdminViewSuppliers";
+import AdminInventoryProducts from "./AdminInventoryProducts";
+import AdminRegisteredUser from "./AdminRegisteredUsers";
+import AdminDiscount from "./AdminDiscount";
+import AdminSalesReport from "./SalesReport";
+import LetsLogin from "./TheLogin";
 import AdminInventoryAddForm from './AdminInventoryAddForm';
-import AdminViewDiscount from './AdminViewDiscount';
-import Cart from './Cart';
-import AppMain from './AppMain'
-import { Col, Container, Row } from 'react-bootstrap';
 
+let API_URL = "44392";
 
+// class App extends React.Component {
+
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       isloggedin: false,
+//       homeScreenButton: true,
+//       isloginclicked: false,
+//       AdminLogginButtonclicked: false,
+//       isAdmin: false,
+//       search: "",
+//       AdminPannelComponent: <AdminInventoryProducts API_URL={API_URL} className = "adminSales"/>
+//     }
+
+//     this.loginbuttonhandler = this.loginbuttonhandler.bind(this);
+//     this.AdminLoginButtonHandler = this.AdminLoginButtonHandler.bind(this);
+//     this.HomeScreenButtonHandler = this.HomeScreenButtonHandler.bind(this);
+
+//     this.viewInventoryButtonHandler = this.viewInventoryButtonHandler.bind(this);
+//     this.addSupplierButtonHandler = this.addSupplierButtonHandler.bind(this);
+//     this.viewSupplierButtonHandler = this.viewSupplierButtonHandler.bind(this);
+//     this.registeredUserButtonHandler = this.registeredUserButtonHandler.bind(this);
+//     this.discountButtonHandler = this.discountButtonHandler.bind(this);
+//     this.SalesReportButtonHandler = this.SalesReportButtonHandler.bind(this);
+//   }
+
+//   render(){
+//     if(this.state.homeScreenButton){    //this shows the to a normal user who is not logged in
+//       return(
+//         <div className="App">
+//         <Navigbar
+//           loginbuttonhandler = {this.loginbuttonhandler}
+//           AdminLoginButtonHandler = {this.AdminLoginButtonHandler}
+//           HomeScreenButtonHandler = {this.HomeScreenButtonHandler}
+//         />
+//         <Container fluid className="mainContainer">
+//           <Row>
+//             <Col className="leftCol" lg = {4} sm= {12}>
+//               <Sidepanel/>
+//             </Col>
+//               <Col className = "rightColumn">
+//                 <RightColumn API_URL={API_URL}/>
+//             </Col>
+//           </Row>
+//         </Container>
+//       </div>
+//       )
+//     }
+//     if(this.state.isloginclicked){    //this shows the login page after the user clicks the login button
+//       return(
+//         <div>
+//            <LetsLogin/>
+//         </div>
+//       )
+//     }
+//     if(this.state.isAdmin){
+//       return(
+//         <div>
+//           <div className="App">
+//             <Navigbar
+//               loginbuttonhandler = {this.loginbuttonhandler}/>
+//             <Container fluid className="mainContainer">
+//               <Row>
+//                 <Col className="leftCol" lg = {4} sm= {12}>
+//                   <Adminsidepanel
+//                     viewInventoryButtonHandler = {this.viewInventoryButtonHandler}
+//                     addSupplierButtonHandler = {this.addSupplierButtonHandler}
+//                     viewSupplierButtonHandler = {this.viewSupplierButtonHandler}
+//                     registeredUserButtonHandler = {this.registeredUserButtonHandler}
+//                     discountButtonHandler = {this.discountButtonHandler}
+//                     SalesReportButtonHandler = {this.SalesReportButtonHandler}
+//                   />
+//                 </Col>
+//                 <Col className = "admincol">
+//                   {this.state.AdminPannelComponent}
+//                 </Col>
+//               </Row>
+//             </Container>
+//           </div>
+//         </div>
+//       )
+//     }
+
+//   }
+// //----------------------------------------------------------------------------------------------------------------------------------------
+//   viewInventoryButtonHandler(){
+//     this.setState({
+//     AdminPannelComponent: <AdminInventoryProducts API_URL = {API_URL} />
+//     });
+//   }
+//   addSupplierButtonHandler(){
+//     this.setState({
+//       AdminPannelComponent: <AdminSupplierAddForm API_URL={API_URL} />
+//       });
+//   }
+//   viewSupplierButtonHandler(){
+//     this.setState({
+//       AdminPannelComponent: <AdminViewSupplier API_URL={API_URL} />
+//       });
+//   }
+//   registeredUserButtonHandler(){
+//     this.setState({
+//       AdminPannelComponent: <AdminRegisteredUser API_URL={API_URL} />
+//       });
+//   }
+//   discountButtonHandler(){
+//     this.setState({
+//       AdminPannelComponent: <AdminDiscount API_URL={API_URL} />
+//       });
+//   }
+//   SalesReportButtonHandler(){
+//     this.setState({
+//       AdminPannelComponent: <AdminSalesReport API_URL={API_URL} />
+//       });
+//   }
+
+// //----------------------------------------------------------------------------------------------------------------------------------------
+//   HomeScreenButtonHandler(){
+//       this.setState({
+//         isAdmin: false,
+//         homeScreenButton:true
+//       });
+//   }
+
+//   AdminLoginButtonHandler(){
+//       this.setState({
+//         homeScreenButton: false,
+//         isAdmin: true
+//       });
+//   }
+//   loginbuttonhandler(){
+//     if(!this.state.isloginclicked){
+//       this.setState({
+//         isloginclicked: true,
+//       });
+//     }
+//   }
+// //----------------------------------------------------------------------------------------------------------------------------------------
+// }
 class App extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-
     this.state = {
       isloggedin: false,
       homeScreenButton: true,
       isloginclicked: false,
       AdminLogginButtonclicked: false,
-      cartButtonNavBarClicked: false,
       isAdmin: false,
       search: "",
-      AdminPannelComponent: <AdminInventoryProducts className = "adminSales"/>
-    }
+      screennumber: 1,
+      AdminPannelComponent: (
+        <AdminInventoryProducts API_URL={API_URL} className="adminSales" />
+      ),
+    };
 
     this.loginbuttonhandler = this.loginbuttonhandler.bind(this);
     this.AdminLoginButtonHandler = this.AdminLoginButtonHandler.bind(this);
     this.HomeScreenButtonHandler = this.HomeScreenButtonHandler.bind(this);
-    this.cartButtonHandler = this.cartButtonHandler.bind(this);
 
-    this.viewInventoryButtonHandler = this.viewInventoryButtonHandler.bind(this);
-    this.addInventoryButtonHandler = this.addInventoryButtonHandler.bind(this);
+    this.viewInventoryButtonHandler =
+      this.viewInventoryButtonHandler.bind(this);
+      this.addInventoryButtonHandler = this.addInventoryButtonHandler.bind(this);
     this.addSupplierButtonHandler = this.addSupplierButtonHandler.bind(this);
     this.viewSupplierButtonHandler = this.viewSupplierButtonHandler.bind(this);
-    this.registeredUserButtonHandler = this.registeredUserButtonHandler.bind(this);
+    this.registeredUserButtonHandler =
+      this.registeredUserButtonHandler.bind(this);
     this.discountButtonHandler = this.discountButtonHandler.bind(this);
-    this.viewdiscountButtonHandler = this.viewdiscountButtonHandler.bind(this);
+    this.SalesReportButtonHandler = this.SalesReportButtonHandler.bind(this);
   }
 
-  render(){
-    if(this.state.homeScreenButton){    //this shows the to a normal user who is not logged in 
-      
-      return(
+  render() {
+    if (this.state.screennumber == 1) {
+      //this shows the to a normal user who is not logged in
+      return (
         <div className="App">
-        <Navigbar 
-          loginbuttonhandler = {this.loginbuttonhandler} 
-          AdminLoginButtonHandler = {this.AdminLoginButtonHandler} 
-          HomeScreenButtonHandler = {this.HomeScreenButtonHandler}
-          cartButtonHandler = {this.cartButtonHandler}
-        />
-              <AppMain/>
-
-
-      </div>
-      )
-    }
-    if(this.state.isloginclicked){    //this shows the login page after the user clicks the login button
-      return(
-        <div>
-           <LetsLogin/>
+          <Navigbar
+            loginbuttonhandler={this.loginbuttonhandler}
+            AdminLoginButtonHandler={this.AdminLoginButtonHandler}
+            HomeScreenButtonHandler={this.HomeScreenButtonHandler}
+          />
+          <Container fluid className="mainContainer">
+            <Row>
+              <Col className="leftCol" lg={4} sm={12}>
+                <Sidepanel />
+              </Col>
+              <Col className="rightColumn">
+                <RightColumn API_URL={API_URL} />
+              </Col>
+            </Row>
+          </Container>
         </div>
-      )
+      );
     }
-    if(this.state.isAdmin){
-      return(
+    if (this.state.screennumber == 2) {
+      //this shows the login page after the user clicks the login button
+      return (
+        <div>
+          <LetsLogin />
+        </div>
+      );
+    }
+    if (this.state.screennumber == 3) {
+      return (
         <div>
           <div className="App">
-            <Navigbar 
-              loginbuttonhandler = {this.loginbuttonhandler}/>
+            <Navigbar
+              loginbuttonhandler={this.loginbuttonhandler}
+              AdminLoginButtonHandler={this.AdminLoginButtonHandler}
+              HomeScreenButtonHandler={this.HomeScreenButtonHandler}
+            />
             <Container fluid className="mainContainer">
               <Row>
-                <Col className="leftCol" lg = {4} sm= {12}>
-                  <Adminsidepanel 
+                <Col className="leftCol" lg={4} sm={12}>
+                  <Adminsidepanel
                     viewInventoryButtonHandler = {this.viewInventoryButtonHandler}
                     addInventoryButtonHandler = {this.addInventoryButtonHandler}
                     addSupplierButtonHandler = {this.addSupplierButtonHandler}
@@ -90,100 +241,74 @@ class App extends React.Component {
                     viewdiscountButtonHandler = {this.viewdiscountButtonHandler}
                   />
                 </Col>
-                <Col className = "admincol">
+                <Col className="admincol">
                   {this.state.AdminPannelComponent}
                 </Col>
               </Row>
             </Container>
           </div>
         </div>
-      )
+      );
     }
-    if (this.state.cartButtonNavBarClicked){
-      return(
-      <div className="App">
-        <div>In cart</div>
-        <Navigbar 
-          loginbuttonhandler = {this.loginbuttonhandler} 
-          AdminLoginButtonHandler = {this.AdminLoginButtonHandler} 
-          HomeScreenButtonHandler = {this.HomeScreenButtonHandler}
-          cartButtonHandler = {this.cartButtonHandler}
-        />
-        <Cart/>
-      </div>
-      )
-    }
-    
-    
   }
-//----------------------------------------------------------------------------------------------------------------------------------------
-  viewInventoryButtonHandler(){
+  //----------------------------------------------------------------------------------------------------------------------------------------
+  viewInventoryButtonHandler() {
     this.setState({
-    AdminPannelComponent: <AdminInventoryProducts/>
+      AdminPannelComponent: <AdminInventoryProducts API_URL={API_URL} />,
     });
   }
   addInventoryButtonHandler(){
     this.setState({
-    AdminPannelComponent: <AdminInventoryAddForm/>
+    AdminPannelComponent: <AdminInventoryAddForm API_URL={API_URL}/>,
+    isAdmin:true,
+    homeScreenButton:false,  
+  });
+  }
+  addSupplierButtonHandler() {
+    this.setState({
+      AdminPannelComponent: <AdminSupplierAddForm API_URL={API_URL} />,
     });
   }
-  addSupplierButtonHandler(){
+  viewSupplierButtonHandler() {
     this.setState({
-      AdminPannelComponent: <AdminSupplierAddForm/>
-      });
+      AdminPannelComponent: <AdminViewSupplier API_URL={API_URL} />,
+    });
   }
-  viewSupplierButtonHandler(){
+  registeredUserButtonHandler() {
     this.setState({
-      AdminPannelComponent: <AdminViewSupplier/>
-      });
+      AdminPannelComponent: <AdminRegisteredUser API_URL={API_URL} />,
+    });
   }
-  registeredUserButtonHandler(){
+  discountButtonHandler() {
     this.setState({
-      AdminPannelComponent: <AdminRegisteredUser/>
-      });
+      AdminPannelComponent: <AdminDiscount API_URL={API_URL} />,
+    });
   }
-  discountButtonHandler(){
+  SalesReportButtonHandler() {
     this.setState({
-      AdminPannelComponent: <AdminDiscount/>
-      });
-  }
-  viewdiscountButtonHandler(){
-    this.setState({
-      AdminPannelComponent: <AdminViewDiscount/>
-      });
+      AdminPannelComponent: <AdminSalesReport API_URL={API_URL} />,
+    });
   }
 
-//---------------------------------------------------------------------------------------------------------------------------------------- 
-  HomeScreenButtonHandler(){
-      this.setState({
-        isAdmin: false,
-        homeScreenButton:true
-      });
+  //----------------------------------------------------------------------------------------------------------------------------------------
+  HomeScreenButtonHandler() {
+    this.setState({
+      screennumber: 1,
+    });
   }
 
-  AdminLoginButtonHandler(){
-      this.setState({
-        homeScreenButton: false,
-        isAdmin: true
-      });
+  AdminLoginButtonHandler() {
+    this.setState({
+      screennumber: 3,
+    });
   }
-  loginbuttonhandler(){
-    if(!this.state.isloginclicked){
-      this.setState({
-        isloginclicked: true,
-      });
-    }
+  loginbuttonhandler() {
+    this.setState({
+      screennumber: 2,
+    });
   }
-  cartButtonHandler(){
-    if(!this.state.isloginclicked){
-      this.setState({
-        homeScreenButton: false,
-        cartButtonNavBarClicked:true,
-      });
-    }
-  }
-//----------------------------------------------------------------------------------------------------------------------------------------
+
+  //----------------------------------------------------------------------------------------------------------------------------------------
 }
 
 export default App;
-
